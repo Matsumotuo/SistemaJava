7c765e58fa24308fdaa79617060113fae11fb35d
useroname23 <lopeshenrique973@gmail.com>
2024-06-24 18:09:17 -0300

    logs

--- SistemaTarefas/src/controller/ExportadorTarefasPorTipo.java
+++ SistemaTarefas/src/controller/ExportadorTarefasPorTipo.java
@@ -1,11 +1,21 @@
 package controller;
 
-import models.*;
-
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
+
+import models.Tarefa;
+import models.TarefaAlimentacao;
+import models.TarefaDataComemorativa;
+import models.TarefaEstudo;
+import models.TarefaExercicio;
+import models.TarefaFinanceira;
+import models.TarefaLazer;
+import models.TarefaPessoal;
+import models.TarefaProjetos;
+import models.TarefaTrabalho;
+import models.TarefaUrgente;
 
 public class ExportadorTarefasPorTipo {
 
@@ -15,6 +25,11 @@ public class ExportadorTarefasPorTipo {
         int totalTrabalho = 0;
         int totalLazer = 0;
         int totalEstudo = 0;
+        int totalAlimento = 0;
+        int totalNiver = 0;
+        int totalExer = 0;
+        int totalFinanc = 0;
+        int totalProjeto = 0;
 
         try (PrintWriter writer = new PrintWriter(new FileWriter(nomeArquivo))) {
             writer.println("Lista de Tarefas por Tipo:");
@@ -51,14 +66,52 @@ public class ExportadorTarefasPorTipo {
                     writer.println("Disciplina: " + ((TarefaEstudo) tarefa).getDisciplina());
                     writer.println();
                     totalEstudo++;
+                }else if (tarefa instanceof TarefaAlimentacao) {
+                    writer.println("Tarefa de Estudo: " + tarefa.getTitulo());
+                    writer.println("Descrição: " + tarefa.getDescricao());
+                    writer.println("Disciplina: " + ((TarefaEstudo) tarefa).getDisciplina());
+                    writer.println();
+                    totalAlimento++;
+                }
+                else if (tarefa instanceof TarefaDataComemorativa) {
+                    writer.println("Tarefa de Estudo: " + tarefa.getTitulo());
+                    writer.println("Descrição: " + tarefa.getDescricao());
+                    writer.println("Disciplina: " + ((TarefaEstudo) tarefa).getDisciplina());
+                    writer.println();
+                    totalNiver++;
+                }
+                else if (tarefa instanceof TarefaExercicio) {
+                    writer.println("Tarefa de Estudo: " + tarefa.getTitulo());
+                    writer.println("Descrição: " + tarefa.getDescricao());
+                    writer.println("Disciplina: " + ((TarefaEstudo) tarefa).getDisciplina());
+                    writer.println();
+                    totalExer++;
+                }else if (tarefa instanceof TarefaFinanceira) {
+                    writer.println("Tarefa de Estudo: " + tarefa.getTitulo());
+                    writer.println("Descrição: " + tarefa.getDescricao());
+                    writer.println("Disciplina: " + ((TarefaEstudo) tarefa).getDisciplina());
+                    writer.println();
+                    totalFinanc++;
+                }else if (tarefa instanceof TarefaProjetos) {
+                    writer.println("Tarefa de Estudo: " + tarefa.getTitulo());
+                    writer.println("Descrição: " + tarefa.getDescricao());
+                    writer.println("Disciplina: " + ((TarefaEstudo) tarefa).getDisciplina());
+                    writer.println();
+                    totalProjeto++;
                 }
             }
+            
 
             writer.println("Total de Tarefas Urgentes: " + totalUrgentes);
             writer.println("Total de Tarefas Pessoais: " + totalPessoais);
             writer.println("Total de Tarefas de Trabalho: " + totalTrabalho);
             writer.println("Total de Tarefas de Lazer: " + totalLazer);
             writer.println("Total de Tarefas de Estudo: " + totalEstudo);
+            writer.println("Total de Tarefas de Alimentação: " + totalAlimento);
+            writer.println("Total de Tarefas de Datas comemorativas: " + totalNiver);
+            writer.println("Total de Tarefas de Exercicios: " + totalExer);
+            writer.println("Total de Tarefas de Financeiro: " + totalFinanc);
+            writer.println("Total de Tarefas dos Projetos: " + totalProjeto);
 
             System.out.println("Lista de tarefas exportada para o arquivo: " + nomeArquivo);
         } catch (IOException e) {
